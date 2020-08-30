c481b75b7818d33781ae68da8eb1cffaf6ab839c
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-08-30 11:49:23 +0300

    configure httpd rewrite engine

--- start.sh
+++ start.sh
@@ -2,6 +2,8 @@
 
 source .env
 
+./stop.sh
+
 download_dump() {
   local users="https://dtapi.if.ua/~yurkovskiy/dtapi_full.sql"
   local sessions="https://dtapi.if.ua/~yurkovskiy/IF-108/sessions.sql"
@@ -19,21 +21,21 @@ dockerize() {
     -e MYSQL_PASSWORD="$MYSQL_PASSWORD" \
     -e MYSQL_ROOT_PASSWORD="$MYSQL_PASSWORD" \
     -e MYSQL_DATABASE="$MYSQL_DATABASE" \
-    -v "$BASEDIR/db_container:/docker-entrypoint-initdb.d" \
+    -v "$BASEDIR/db_container:/docker-entrypoint-initdb.d:ro" \
     -d mysql:5.7
 
-  docker build -t "$DOCKER_BE_IMAGE" .
+  docker build -t "$DOCKER_BE_IMAGE" -f be_container/Dockerfile .
 
   docker run --name "$DOCKER_BE" \
     -p 8001:80 \
     --network "$DOCKER_NETWORK" \
-    -v "$APP_BE:/var/www/html" \
     -d "$DOCKER_BE_IMAGE"
 
   docker run --name "$DOCKER_FE" \
     -p 8002:80 \
     --network "$DOCKER_NETWORK" \
-    -v "$APP_FE:/usr/local/apache2/htdocs/" \
+    -v "$APP_FE:/usr/local/apache2/htdocs/:ro" \
+    -v "$BASEDIR/fe_container/httpd.conf:/usr/local/apache2/conf/httpd.conf:ro" \
     -d httpd:2.4.46-alpine
 
   docker run --name "$DOCKER_LB" \
