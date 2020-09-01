5dd9520c4f4a6206c89bc94a8ed101b766a1faa7
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-09-01 19:41:02 +0300

    update docker be

--- be_container/docker-entrypoint.sh
+++ be_container/docker-entrypoint.sh
@@ -3,4 +3,4 @@ sed -i -e "s|RewriteBase /|RewriteBase /api|g" "$APP/.htaccess"
 sed -i -e "s|'base_url'   => '/'|'base_url'   => '/api/'|g" "$APP/application/bootstrap.php"
 sed -i -e "s|localhost|$MYSQL_HOST|g" "$APP/application/config/database.php"
 
-apache2-foreground
+exec "$@"
