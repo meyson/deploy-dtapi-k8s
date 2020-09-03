0d1b419bb8c5dc8340eb4a5998a944d204b1281c
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-09-03 09:17:52 +0300

    change service type for be and fe

--- db_container/mysql_config.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-BASE_DIR="/docker-entrypoint-initdb.d"
-mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < "$BASE_DIR/dtapi_full.sql"
-mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" "$MYSQL_DATABASE" < "$BASE_DIR/sessions.sql"
