2d274f6ea00ca7ffd72cf259c3c5bd5ab5497a51
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-09-09 22:25:46 +0300

    add k8s init script

---
+++ k8s/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+
+# Create GCE disk
+gcloud compute disks create dtapi-mysql --zone=europe-north1-a --size=10
+kubectl apply -f "$BASEDIR/gcp/compute-address.yaml"
+kubectl apply -k "$BASEDIR"
