5c66b01a37d333e355c8b8b69499f7921837c30c
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-09-10 22:41:07 +0300

    divide k8 files

--- k8s/init.sh
+++ k8s/init.sh
@@ -5,6 +5,6 @@ BASEDIR=$(dirname "$0")
 # Create GCE disk
 gcloud compute disks create dtapi-mysql --zone=europe-north1-a --size=10
 
-kubectl apply -f "$BASEDIR/gcp/dtapi-cerf.yaml"
+kubectl apply -f "$BASEDIR/gcp/TLS-cerf.yaml"
 kubectl apply -f "$BASEDIR/gcp/compute-address.yaml"
 kubectl apply -k "$BASEDIR"
